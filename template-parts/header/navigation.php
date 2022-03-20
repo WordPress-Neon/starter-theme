01d585ed4bb905c6429ac6fd48eb15f6c654c186
Josh <josh@bonnick.dev>
2022-03-20 21:35:34 +0000

    Initial commit

---
+++ template-parts/header/navigation.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul class="flex items-center space-x-3">
+        <li>
+            <a href="<?= home_url('/'); ?>">Home</a>
+        </li>
+        <li>
+            <a href="<?= admin_url(); ?>">WP Admin</a>
+        </li>
+    </ul>
+</nav>
